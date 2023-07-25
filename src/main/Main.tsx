c6fdab97e7f740eb32af2a88724c5ace51d28ba9
AbrarHasin <abrarhasinpranto@gmail.com>
2023-07-25 18:43:08 +0600

    alt prop added in img tag for warning issue

--- src/main/Main.tsx
+++ src/main/Main.tsx
@@ -43,7 +43,7 @@ const Main = () => {
                                 return (
                                     <div className="col-md-4" key={p.id}>
                                         <div className="card mb-4 shadow-sm">
-                                            <img src={p.image} height="180"/>
+                                            <img src={p.image} height="180" alt=""/>
                                             <div className="card-body">
                                                 <p className="card-text">{p.title}</p>
                                                 <div className="d-flex justify-content-between align-items-center">
